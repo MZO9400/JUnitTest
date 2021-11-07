e602115ed46052abb6ddf2e1a237811d66ffb611
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-07 16:37:43 +0500

    Use JUnit.Test class for creating tests

--- src/com/softwaretesting/a1/NumberOperationsTest.kt
+++ src/com/softwaretesting/a1/NumberOperationsTest.kt
@@ -1,34 +1,35 @@
 package com.softwaretesting.a1
 
-import org.junit.jupiter.api.Assertions.assertEquals
+import junit.framework.TestCase.assertEquals
 import kotlin.math.sqrt
+import org.junit.Test;
 
 class NumberOperationsTest {
-    @org.junit.jupiter.api.Test
+    @Test
     fun add() {
         val numberOperations = NumberOperations()
         assertEquals(3, numberOperations.add(1, 2))
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun subtract() {
         val numberOperations = NumberOperations()
         assertEquals(1, numberOperations.subtract(3, 2))
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun multiply() {
         val numberOperations = NumberOperations()
         assertEquals(6, numberOperations.multiply(2, 3))
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun divide() {
         val numberOperations = NumberOperations()
         assertEquals(3, numberOperations.divide(6, 2))
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun isPrime() {
         val numberOperations = NumberOperations()
         assertEquals(true, numberOperations.isPrime(5))
@@ -36,7 +37,7 @@ class NumberOperationsTest {
         assertEquals(true, numberOperations.isPrime(7))
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun distance() {
         val numberOperations = NumberOperations()
         assertEquals(sqrt(13.0), numberOperations.distance(3, 8, 5, 11))
