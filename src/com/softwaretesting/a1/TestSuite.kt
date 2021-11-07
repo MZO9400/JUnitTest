7e53b752eac6f49a1bd234b310c2ad8a7aa05a84
MZO9400 <hamza.hameed00@yahoo.com>
2021-11-07 16:37:57 +0500

    Create a TestSuite

---
+++ src/com/softwaretesting/a1/TestSuite.kt
@@ -0,0 +1,10 @@
+package com.softwaretesting.a1
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import org.junit.runners.Suite.SuiteClasses
+
+
+@RunWith(Suite::class)
+@SuiteClasses(NumberOperationsTest::class)
+class TestSuite
